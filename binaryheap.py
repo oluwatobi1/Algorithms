5cd7c66e0844896bdf14a71759111cb5cfe4fe1f
oluwatobi1 <akintunlesetobi@gmail.com>
2020-06-16 18:16:37 +0100

    adding del func

--- binaryheap.py
+++ binaryheap.py
@@ -19,12 +19,41 @@ class BinHeap:
         self.currentsize +=1
         print(self.currentsize, 'Size')
         self.move(self.currentsize)
-        
-    
-    def getheap(self):
         return self.Heap
         
     
+    
+    def delMin(self):
+        temp = self.Heap[1]
+        self.Heap[1] = self.Heap[self.currentsize]        
+        self.Heap.pop()
+        self.currentsize-=1
+        self.arrange(1)
+        
+    def arrange(self, index):
+        while index*2< self.currentsize:
+            mc = self.minchild(index)
+            
+            if self.Heap[index]>self.Heap[mc]:
+                temp = self.Heap[index]
+                self.Heap[index] = self.Heap[mc]
+                self.Heap[mc] =temp
+                
+            index = mc
+
+    def minchild(self, indx):
+        if indx*2+1>self.currentsize:
+            return indx*2
+        else:
+            if self.Heap[indx*2]>self.Heap[indx*2+1]:
+                return indx*2+1
+            else:
+                return indx*2
+        
+    def getheap(self):
+        return self.Heap
+            
+            
 b = BinHeap()
 b.insert(1)
 b.insert(11)
