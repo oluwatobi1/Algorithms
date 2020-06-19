61502d5a95d6b6425f6de310b4a1faeb6815c8b0
oluwatobi1 <akintunlesetobi@gmail.com>
2020-06-19 19:20:48 +0100

    Two different Approaches implementation

--- validateBST.py
+++ validateBST.py
@@ -1,3 +1,23 @@
+#THis contain two implementations
+### 1. An O(n**2) implementation
+
+def isValidBST1(self, root: TreeNode) -> bool:
+    tree_vals = []
+    def inorder(root):
+        if root:
+            inorder(root.left)
+            tree_vals.append(root.val)
+            inorder(root.right)
+        
+        for i in range(1, len(tree_vals)):
+            if tree_vals[i-1]>=tree_vals[i]:
+                return False
+        return True
+            
+            
+    return inorder(root)
+    
+### 2. An O(n) implementation
 def isValidBST(self, root):
 
         if not root:
