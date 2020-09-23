0b77d609b534d78395c06dc333d864ad5297442a
oluwatobi1 <akintunlesetobi@gmail.com>
2020-09-23 21:06:37 +0100

    Dyn Arr edit

--- Python/Dynamic Array.py
+++ Python/Dynamic Array.py
@@ -10,20 +10,26 @@ class DynamicArray():
         return self.count
 
     def __getitems__(self, index):
-        return self.A[index]
+        return self.Arr[index]
 
     def append(self, item):
         if self.capacity== self.count:
-            self._resize(self.A)
-        self.A[count] = item
+            self._resize(self.Arr)
+        self.Arr[self.count] = item
         self.count+=1
 
-    def _resize(self):
+    def _resize(self, arr):
         self.capacity *=2
         spare = self._make_array(self.capacity)
-        for i in self.count:
-            spare[i]=self.A[i]
-        self.A = spare
+        for i in range(len(arr):
+            spare[i]=arr[i]
+        arr = spare)
     
     def _make_array(self, capacity):
         return (capacity* ctypes.py_object)()
+#Code Test
+m = DynamicArray()
+
+m.append('1')
+m.append('random')
+print(len(m))
