cf75d20677893180b7823ffa2ff40e79b38065fb
oluwatobi1 <akintunlesetobi@gmail.com>
2020-11-10 14:58:59 +0100

    modified Dynamic array

--- Python/Dynamic Array.py
+++ Python/Dynamic Array.py
@@ -1,37 +1,39 @@
 import ctypes
 
-class DynamicArray():
+class DynamicArray(object):
     def __init__(self):
-        self.count = 0
-        self.capacity = 1
-        self.Arr = self._make_array(self.capacity)
-
+        self.size = 0
+        self.cap = 1
+        self.arr = self._make_array(self.cap)
     def __len__(self):
-        return self.count
-
-    def __getitems__(self, index):
-        if 0<index<=self.capacity:
-            return IndexError('Index out of bounds')
-        return self.Arr[index]
-
+        return self.size
     def append(self, item):
-        if self.capacity== self.count:
-            self._resize(self.Arr)
-        self.Arr[self.count] = item
-        self.count+=1
+        if self.size>=self.cap:
+            self.arr = self._increase(self.arr)
+        self.arr[self.size] = item
+        self.size+=1
+    def _increase(self, arr):
+        self.cap = 2*self.cap
+        new_arr = self._make_array(self.cap)
+        for i in range(len(self.arr)):
+            new_arr[i]=self.arr[i]
+        self.arr = new_arr
+        return self.arr    
 
-    def _resize(self, arr):
-        self.capacity *=2
-        spare = self._make_array(self.capacity)
-        for i in range(len(arr)):
-            spare[i]=arr[i]
-            arr = spare
-    
-    def _make_array(self, capacity):
-        return (capacity* ctypes.py_object)()
+    def __getitem__(self, key):
+        return self.arr[key]
+    def _make_array(self, cap):
+        return (cap* ctypes.py_object)()
 #Code Test
 m = DynamicArray()
 
 m.append('1')
 m.append('random')
+m.append('1')
+m.append('random')
+m.append('1')
+m.append('random')
+m.append('1')
+m.append('random')
 print(len(m))
+print(m[:])
