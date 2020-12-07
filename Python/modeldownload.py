5bd571680d6c528991cd5c34bdf655b4590beb07
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-07 15:08:31 +0100

    s3 bucket model download

---
+++ Python/modeldownload.py
@@ -0,0 +1,16 @@
+import boto3
+import botocore
+
+BUCKET_NAME = 'reisparcovidbucket' # replace with your bucket name
+KEY = 'model/efficientnetb6_notop.h5' # replace with your object key
+
+s3 = boto3.resource('s3')
+
+try:
+    print('Downloading')
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 'efficientnetb6_notop.h5')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
