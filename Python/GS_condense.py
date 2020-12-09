fc5ecdb8e320900cd4b3c578593c4db62359cfac
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-09 17:18:20 +0100

    debuged typo:

--- Python/GS_condense.py
+++ Python/GS_condense.py
@@ -1,10 +1,5 @@
-
-#
 # The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
 # The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
-#
-
-#
 # For your reference:
 #
 # SinglyLinkedListNode:
@@ -21,7 +16,7 @@ def condense(head):
     
     while head!=None:        
         if head.data not in store:
-            out.appen`d(head.data)
+            out.append(head.data)
         store.add(head.data)
         head = head.next    
     out_linked = None
@@ -31,8 +26,6 @@ def condense(head):
         temp.next= out_linked
         out_linked=temp
     return out_linked
-
-if __name__ == '__main__':
 
 
 
