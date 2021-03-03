c150fe3dcfc2161b09107f63128026dea8fc7234
oluwatobi1 <akintunlesetobi@gmail.com>
2021-03-03 17:54:29 +0100

    Binary tree Oop  implementation

--- Python/Tutorial_Learn/tree.py
+++ Python/Tutorial_Learn/tree.py
@@ -1,43 +1,41 @@
-def BinaryTree(r):
-    return [r, [],[]]
+class BinaryTree():
+    
+    def __init__(self, root) -> None:
+        self.root = root
+        self.leftchild = None
+        self.rightchild = None
 
-def insertLeft(r, item):
-    t = r.pop(1)
+    def insertLeft(self, leftchild):
+        if not self.leftchild:
+            self.leftchild = BinaryTree(leftchild)
+        else:
+            temp = BinaryTree(self.leftchild)
+            temp.leftchild = self.leftchild
+            self.leftchild = temp
 
-    if len(t)>1:
-        r.insert(1, [item, t, []])
-    else:
-        r.insert(1, [item, [], []])
+    
+    def insertRight(self, rightchild):
+        if not self.rightchild:
+            self.rightchild = BinaryTree(rightchild)
+        else:
+            temp = BinaryTree(self.rightchild)
+            temp.rightchild = self.rightchild
+            self.rightchild = temp
+    def getRightChild(self):
+        return self.rightchild
 
-def insertRight(r, item):
-    t = r.pop(2)
+    def getLeftChild(self):
+        return self.leftchild
 
-    if len(t)>1:
-        r.insert(2, [item, [], t])
-    else:
-        r.insert(2, [item, [], []])
+    def setRootVal(self, key):
+        self.root = key
 
-def getRootval(r):
-    return r[0]
+    def getRootVal(self):
+        return self.root
 
-def setRootval(r, newVal):
-    r[0]=newVal
-
-def getLeftChild(r):
-    return r[1]
-def getRightChild(r):
-    return r[2]
-
-def getTree(r):
-    return r
+b =BinaryTree("a")
+print(b.getRootVal())
     
 
 
-r = BinaryTree(1)
-print(getTree(r))
-insertLeft(r, 3)
-insertLeft(r, 5)
-print(getTree(r))
-insertRight(r, 2)
-insertRight(r, 88)
-print(getTree(r))
+
