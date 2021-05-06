47071fc29f91dbbd21146a206bb70f93b03e71ed
Tobi_HPi7 <akintunlesetobi@gmail.com>
2021-05-06 10:54:24 +0100

    parity 2

--- Python/Tutorial_Learn/EPI/count_bit.py
+++ Python/Tutorial_Learn/EPI/count_bit.py
@@ -9,10 +9,16 @@ def count_bits(x):
     return num_bit
 
 def parity(d):
+    """
+    The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise, it is 0. For
+    example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are used to detect
+    single bit errors in data storage and communication.
+    """
+
     result = 0
     while d:
         result ^= d & 1
         d>>=1
     return result
 
-print(parity(21345))
+print(parity(8392))
