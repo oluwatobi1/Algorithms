2d165e79c8c05a49c24be9717f11b52637428c07
Tobi_HPi7 <akintunlesetobi@gmail.com>
2021-05-08 22:16:59 +0100

    Do and while loop switch flow control

--- typescript/flowControl.ts
+++ typescript/flowControl.ts
@@ -89,3 +89,30 @@ switch (+veh){
     default:
         console.log("Not a Vehicle");        
 }
+
+// while and do
+let i = 0
+while (i<5){
+    console.log(i);
+    i++
+    
+}
+let k = 100
+
+do {
+    console.log(k);
+    k++
+    
+}while (k<105)
+
+let j = 90
+
+do {
+    console.log(j);
+    j++
+    if (j >100){
+        break
+    }
+    
+}while (true)
+
