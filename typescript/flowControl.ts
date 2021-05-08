69ff8880f31258c1f1919e0bbe8fe7f9f92e8a61
Tobi_HPi7 <akintunlesetobi@gmail.com>
2021-05-08 21:30:56 +0100

    flow control typescript

---
+++ typescript/flowControl.ts
@@ -0,0 +1,10 @@
+let numb :number = 12
+// single statement if
+if (numb <6 ) console.log("number is lesser");
+else if (numb >6 && numb <8){
+console.log( "number is within 6 & 8");
+}else {
+    console.log("number is above 8");
+    
+}
+
