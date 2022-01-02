3279b794ef2a04bdbb3294560584a1fdf983ce97
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-02 17:09:07 +0100

    valid palidrome

---
+++ Python/LC/valid_palindrome.py
@@ -0,0 +1,5 @@
+import string
+def isPalindrome(self, s: str) -> bool:
+    # using the walrus operator
+    return (y:=''.join([i for i in s.lower() if i not in string.punctuation+" "]))==y[::-1]
+    
