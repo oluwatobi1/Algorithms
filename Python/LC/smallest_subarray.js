830bed3f93b55750a139035c51cc18cf5e434017
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-05 14:49:07 +0100

    cleanup" "

--- Python/LC/smallest_subarray.js
+++ Python/LC/smallest_subarray.js
@@ -1,19 +1,14 @@
-console.log("Starting;");
 const smallest_subarray_with_given_sum = function(s, arr) {
     // TODO: Write your code here
     let windowSum = 0;
     let windowStart = 0;
     let windowEnd = 0;
     let result;
-    let cache = []
 
     while (windowStart < arr.length && windowEnd < arr.length) {
         windowSum += arr[windowEnd]
-        cache.push(arr[windowEnd])
-        console.log("result ", result, cache, windowSum);
         if (windowSum >= s) {
             curr_result = arr.slice(windowStart, windowEnd + 1)
-            console.log("true", windowSum, "c", windowStart, windowEnd, windowSum, curr_result);
             if (!result || curr_result.length < result.length) {
                 result = curr_result
             }
@@ -21,10 +16,8 @@ const smallest_subarray_with_given_sum = function(s, arr) {
             windowSum -= arr[windowEnd]
             windowStart += 1
         } else {
-            console.log("else");
             if (windowEnd < arr.length) {
                 windowEnd += 1
-                console.log("move", windowEnd);
             };
         }
     }
