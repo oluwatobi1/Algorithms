283a6836f540c2ad76ca91e73475e49d55c895c4
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-06 19:14:53 +0100

    added comment

--- Python/LC/Longest Substring(Same Letters).js
+++ Python/LC/Longest Substring(Same Letters).js
@@ -1,3 +1,18 @@
+// Given a string with lowercase letters only, if you are allowed to replace no more than k letters with any letter, find the length of the longest substring having the same letters after replacement.
+
+// Input: String="aabccbb", k=2
+// Output: 5
+// Explanation: Replace the two 'c' with 'b' to have the longest repeating substring "bbbbb".
+
+
+// Input: String="abbcb", k=1
+// Output: 4
+// Explanation: Replace the 'c' with 'b' to have the longest repeating substring "bbbb".
+
+// Input: String="abccde", k=1
+// Output: 3
+// Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
 const length_of_longest_substring = function(str, k) {
     // TODO: Write your code here
     let windowStart = 0,
