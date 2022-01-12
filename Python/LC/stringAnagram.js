9debeed6751521cb0f9cb768d8080a68aa956558
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-12 07:10:03 +0100

    refactor:removed logs

--- Python/LC/stringAnagram.js
+++ Python/LC/stringAnagram.js
@@ -1,4 +1,13 @@
 // Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+// Input: String="ppqp", Pattern="pq"
+// Output: [1, 2]
+// Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+
+// Input: String="abbcabc", Pattern="abc"
+// Output: [2, 3, 4]
+// Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+
 const find_string_anagrams = function(str, pattern) {
     let result_indexes = [],
         windowStart = 0,
@@ -10,38 +19,28 @@ const find_string_anagrams = function(str, pattern) {
         windowSeen[rightChar] = (windowSeen[rightChar] || 0) + 1
     }
     // TODO: Write your code here
-    console.log(" Start windowSeen", windowSeen);
 
     for (i = 0; i < str.length; i++) {
         let currChar = str[i]
         if (i > pattern.length - 1) {
             let leftChar = str[windowStart];
-            console.log("leftchar", leftChar, windowSeen);
             if (leftChar in windowSeen) {
                 windowSeen[leftChar] += 1
                 if (windowSeen[leftChar] > 0) {
                     matched -= 1
                 }
-                console.log('////windo', windowSeen, "matched", matched);
             }
             windowStart += 1
         }
-        console.log("firest window", windowSeen, "currChar", currChar, "i", i);
         if (currChar in windowSeen) {
             windowSeen[currChar] -= 1
             if (windowSeen[currChar] > -1) {
                 matched += 1
             }
-            console.log("matched", matched);
         }
         if (matched === pattern.length) {
             result_indexes.push(i - matched + 1)
-            console.log("matched", matched, "pattern", pattern.length, "i", i, 'str', str, 'result', result_indexes);
         }
-
-
-        console.log("..Window Seen ", windowSeen,
-            'window', str.slice(windowStart, i), "matched", matched);
     }
     return result_indexes;
 };
