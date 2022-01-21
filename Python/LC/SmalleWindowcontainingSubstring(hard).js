4e1c1e5f7ac5760dd30f272ab99bc2f1dee6f884
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-21 12:47:32 +0100

    Smallest window containing substring
    "
    gi
    
    "

--- Python/LC/SmalleWindowcontainingSubstring(hard).js
+++ Python/LC/SmalleWindowcontainingSubstring(hard).js
@@ -44,11 +44,16 @@ const find_substring = function(str, pattern) {
             }
         }
 
-        if (matched == pattern.length) {
+        while (matched == pattern.length) {
+            let char = str[windowStart]
             c = str.slice(windowStart, windowEnd + 1)
             if (!result || c.length < result) {
                 result = c
             }
+            if (windowSeen[char]) {
+                matched -= 1
+            }
+            windowStart += 1
         };
         windowEnd++
     }
@@ -56,7 +61,60 @@ const find_substring = function(str, pattern) {
     return result;
 }
 
+// function find_substring(str, pattern) {
+//     let windowStart = 0,
+//         matched = 0,
+//         substrStart = 0,
+//         minLength = str.length + 1,
+//         charFrequency = {};
+
+//     for (i = 0; i < pattern.length; i++) {
+//         const chr = pattern[i];
+//         if (!(chr in charFrequency)) {
+//             charFrequency[chr] = 0;
+//         }
+//         charFrequency[chr] += 1;
+//     }
+
+//     // try to extend the range [windowStart, windowEnd]
+//     for (windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//         const rightChar = str[windowEnd];
+//         if (rightChar in charFrequency) {
+//             charFrequency[rightChar] -= 1;
+//             if (charFrequency[rightChar] >= 0) { // Count every matching of a character
+//                 matched += 1;
+//             }
+//         }
+
+//         // Shrink the window if we can, finish as soon as we remove a matched character
+//         while (matched === pattern.length) {
+//             if (minLength > windowEnd - windowStart + 1) {
+//                 minLength = windowEnd - windowStart + 1;
+//                 substrStart = windowStart;
+//             }
+
+//             const leftChar = str[windowStart];
+//             windowStart += 1;
+//             if (leftChar in charFrequency) {
+//                 // Note that we could have redundant matching characters, therefore we'll decrement the
+//                 // matched count only when a useful occurrence of a matched character is going out of the window
+//                 if (charFrequency[leftChar] === 0) {
+//                     matched -= 1;
+//                 }
+//                 charFrequency[leftChar] += 1;
+//             }
+//         }
+//     }
+
+//     if (minLength > str.length) {
+//         return '';
+//     }
+//     return str.substring(substrStart, substrStart + minLength);
+// }
+
+
 
 console.log("Result ", find_substring('aabdec', 'abc'));
 console.log("Result ", find_substring('abdbca', 'abc'));
 console.log("Result ", find_substring('adcad', 'abc'));
+console.log("Result ", find_substring('absibdibsibwbs', 'bdb'));
