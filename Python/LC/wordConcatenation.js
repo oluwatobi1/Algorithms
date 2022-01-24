ba3515994a7dbe4901770835f5e5240e50451425
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-24 13:42:12 +0100

    solution review

--- Python/LC/wordConcatenation.js
+++ Python/LC/wordConcatenation.js
@@ -35,6 +35,77 @@ const find_word_concatenation = function(str, words) {
     return result_indices;
 };
 
+
+
+// const find_word_concatenation = function(str, words) {
+//     if (words.length === 0 || words[0].length === 0) {
+//         return []
+//     }
+//     // initialize hash map
+//     let wordFrequency = {};
+//     const result = [];
+//     // add all item in word to hash map
+//     words.forEach((word) => {
+//         wordFrequency[word] = (wordFrequency[word] || 0) + 1
+//     })
+//     // initialize wordsCount and wordLength
+//     let wordsCount = words.length,
+//     wordLen
+
+// };
+
+
+// function find_word_concatenation(str, words) {
+//     if (words.length === 0 || words[0].length === 0) {
+//         return [];
+//     }
+
+//     wordFrequency = {};
+
+//     words.forEach((word) => {
+//         if (!(word in wordFrequency)) {
+//             wordFrequency[word] = 0;
+//         }
+//         wordFrequency[word] += 1;
+//     });
+
+//     const resultIndices = [],
+//         wordsCount = words.length;
+//     wordLength = words[0].length;
+
+//     for (i = 0; i < (str.length - wordsCount * wordLength) + 1; i++) {
+//         const wordsSeen = {};
+//         for (j = 0; j < wordsCount; j++) {
+//             next_word_index = i + j * wordLength;
+//             // Get the next word from the string
+//             word = str.substring(next_word_index, next_word_index + wordLength);
+//             if (!(word in wordFrequency)) { // Break if we don't need this word
+//                 break;
+//             }
+
+//             // Add the word to the 'wordsSeen' map
+//             if (!(word in wordsSeen)) {
+//                 wordsSeen[word] = 0;
+//             }
+//             wordsSeen[word] += 1;
+
+
+//             // no need to process further if the word has higher frequency than required
+//             if (wordsSeen[word] > (wordFrequency[word] || 0)) {
+//                 break;
+//             }
+
+//             if (j + 1 === wordsCount) { // Store index if we have found all the words
+//                 resultIndices.push(i);
+//             }
+//         }
+//     }
+
+//     return resultIndices;
+// }
+
+
+
 console.log("RESULT ", find_word_concatenation("catfoxcat", ["cat", "fox"]))
 console.log("RESULT ", find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
 console.log("RESULT ", find_word_concatenation("catcatfoxsheepfox", ["cat", "fox", "sheep"]))
