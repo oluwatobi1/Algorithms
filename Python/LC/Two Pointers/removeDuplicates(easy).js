d39b8bf75979bfc1763a0e7dc7eb57d7107446a5
oluwatobi1 <akintunlesetobi@gmail.com>
2022-01-24 17:47:43 +0100

    remove duplicates

---
+++ Python/LC/Two Pointers/removeDuplicates(easy).js
@@ -0,0 +1,22 @@
+// Given an array of sorted numbers, remove all duplicates from it.
+//  You should not use any extra space; after removing the duplicates in-place
+//  return the length of the subarray that has no duplicate in it.
+
+// Input: [2, 3, 3, 3, 6, 9, 9]
+// Output: 4
+// Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+
+// Input: [2, 2, 2, 11]
+// Output: 2
+// Explanation: The first two elements after removing the duplicates will be [2, 11].
+
+
+const remove_duplicates = function(arr) {
+    // TODO: Write your code here
+
+    return -1;
+};
+
+
+console.log(remove_duplicates([2, 3, 3, 3, 6, 9, 9]));
+console.log(remove_duplicates([2, 2, 2, 11]));
