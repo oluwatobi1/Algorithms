1ff32ba06f4fd15f95c124e99c1f7cfe1d528d40
oluwatobi1 <akintunlesetobi@gmail.com>
2022-02-10 13:46:42 +0100

    comment

---
+++ Python/LC/Two Pointers/dutchNationalFlagProblem.js
@@ -0,0 +1,7 @@
+// Given an array containing 0s, 1s and 2s, sort the array in-place.
+//  You should treat numbers of the array as objects, hence, we can’t count 0s, 1s,
+// and 2s to recreate the array.
+
+// The flag of the Netherlands consists of three colors: red, white and blue; 
+// and since our input array also consists of three different numbers that is 
+// why it is called Dutch National Flag problem.
