b922a319f6ec93b538d4946b2ec2d314da4c80c7
oluwatobi1 <akintunlesetobi@gmail.com>
2022-03-15 23:51:04 +0100

    chore: refactor arrangement

--- Python/LC/sliding window/567. Permutation in String.py
+++ Python/LC/sliding window/567. Permutation in String.py
@@ -10,35 +10,34 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         seen, temp = {}, {}
         for char in s1:
-            seen[char]= seen.get(char, 0)+1
-            temp[char]=temp.get(char, 0)+1           
-            
+            seen[char] = seen.get(char, 0)+1
+            temp[char] = temp.get(char, 0)+1
+
         window_start = 0
         window_end = 0
-        while window_end<len(s2):
+        while window_end < len(s2):
             curr_char = s2[window_end]
             if seen.get(curr_char, False):
                 if temp.get(curr_char, False):
-                    temp[curr_char]-=1
-                    if temp[curr_char]==0:
+                    temp[curr_char] -= 1
+                    if temp[curr_char] == 0:
                         del temp[curr_char]
                 else:
-                    temp[curr_char]=temp.get(curr_char, 0)-1    
-                    
-            if window_end>len(s1)-1:
+                    temp[curr_char] = temp.get(curr_char, 0)-1
+
+            if window_end > len(s1)-1:
                 leaving_char = s2[window_start]
                 if seen.get(leaving_char, False):
                     if temp.get(leaving_char, False):
-                        temp[leaving_char]+=1                        
-                        if temp[leaving_char]==0:
+                        temp[leaving_char] += 1
+                        if temp[leaving_char] == 0:
                             del temp[leaving_char]
                     else:
-                        temp[leaving_char]=temp.get(leaving_char, 0)+1
-                        
-                window_start+=1
-            if len(temp)<1:
+                        temp[leaving_char] = temp.get(leaving_char, 0)+1
+
+                window_start += 1
+            if len(temp) < 1:
                 return True
-            window_end+=1           
-            
+            window_end += 1
+
         return False
-                
--- Python/LC/sliding window/valid_palindrome.py
+++ Python/LC/sliding window/valid_palindrome.py
@@ -1,5 +1,6 @@
 import string
+
+
 def isPalindrome(self, s: str) -> bool:
     # using the walrus operator
-    return (y:=''.join([i for i in s.lower() if i not in string.punctuation+" "]))==y[::-1]
-    
+    return (y := ''.join([i for i in s.lower() if i not in string.punctuation+" "])) == y[::-1]
