0da2c288b0c968d74337a4f5bfdf83f9d08aa547
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-16 17:36:35 +0100

    solution

--- Python/LC/sliding window/fast and slow pointer/cycle in cylic linkedlist.js
+++ Python/LC/sliding window/fast and slow pointer/cycle in cylic linkedlist.js
@@ -32,12 +32,44 @@
 // Output: false
 // Explanation: The array does not have any cycle.
 
-
 const circular_array_loop_exists = function(arr) {
     // TODO: Write your code here
+    for (let i = 0; i < arr.length; i++) {
+        let isForward = arr[i] >= 0;
+        let fast = i,
+            slow = i;
+        while (true) {
+            slow = find_next_index(arr, isForward, slow);
+            console.log('slow', slow)
+            fast = find_next_index(arr, isForward, fast);
+
+            if (fast !== -1) {
+                fast = find_next_index(arr, isForward, fast);
+            };
+            if (slow === -1 || fast === -1 || slow === fast) {
+                break;
+            };
+        }
+        if (slow !== -1 && slow === fast) {
+            return true
+        }
+    }
     return false;
 };
 
+const find_next_index = function(arr, isForward, currentIndex) {
+    let direction = arr[currentIndex] >= 0;
+    if (direction !== isForward) return -1;
+
+    let nextIndex = (currentIndex + arr[currentIndex]) % arr.length
+    if (nextIndex < 0) {
+        nextIndex = nextIndex + arr.length;
+    };
+    if (nextIndex === currentIndex) {
+        nextIndex = -1
+    }
+    return nextIndex
+}
 
 
 console.log(`${circular_array_loop_exists([1, 2, -1, 2, 2])}`)
