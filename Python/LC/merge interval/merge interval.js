13c19c268a91dd07f0af9c71d7426f220cc47af3
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-18 08:39:29 +0100

    solution

--- Python/LC/merge interval/merge interval.js
+++ Python/LC/merge interval/merge interval.js
@@ -1,26 +1,3 @@
-// Given a list of intervals, merge all the overlapping intervals to produce a list 
-// that has only mutually exclusive intervals.
-
-// Example 1:
-
-// Intervals: [[1,4], [2,5], [7,9]]
-// Output: [[1,5], [7,9]]
-// Explanation: Since the first two intervals [1,4] and [2,5] overlap, we merged them into 
-// one [1,5].
-
-// Example 2:
-
-// Intervals: [[6,7], [2,4], [5,9]]
-// Output: [[2,4], [5,9]]
-// Explanation: Since the intervals [6,7] and [5,9] overlap, we merged them into one [5,9].
-
-// Example 3:
-
-// Intervals: [[1,4], [2,6], [3,5]]
-// Output: [[1,6]]
-// Explanation: Since all the given intervals overlap, we merged them into one.
-
-
 class Interval {
     constructor(start, end) {
         this.start = start;
@@ -36,6 +13,23 @@ class Interval {
 const merge = function(intervals) {
     merged = []
         // TODO: Write your code here
+    if (intervals.length < 2) {
+        return intervals
+    }
+    intervals.sort((a, b) => a.start - b.start)
+    let start = intervals[0].start,
+        end = intervals[0].end;
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i].start < end) {
+            end = Math.max(end, intervals[i].end)
+        } else {
+            merged.push(new Interval(start, end));
+            start = intervals[i].start;
+            end = intervals[i].end;
+        }
+    }
+    merged.push(new Interval(start, end))
+
     return merged;
 };
 
