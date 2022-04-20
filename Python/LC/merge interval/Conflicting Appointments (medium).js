4c144e8637c400c85c09f9d40a673300ad0265c4
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-20 19:13:18 +0100

    adds similar problem question

--- Python/LC/merge interval/Conflicting Appointments (medium).js
+++ Python/LC/merge interval/Conflicting Appointments (medium).js
@@ -58,3 +58,14 @@ console.log(`Can attend all appointments: ${can_attend_all_appointments([
     new Interval(2, 3),
     new Interval(3, 6),
   ])}`);
+
+
+//   Similar Problems
+// Problem 1: Given a list of appointments, find all the conflicting appointments.
+
+// Example:
+
+// Appointments: [[4,5], [2,3], [3,6], [5,7], [7,8]]
+// Output: 
+// [4,5] and [3,6] conflict. 
+// [3,6] and [5,7] conflict.
