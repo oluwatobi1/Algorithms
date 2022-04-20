12a9362344a9551cf1057fc93c6a55f4b5ec890e
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-20 19:03:02 +0100

    add working soln

--- Python/LC/merge interval/Conflicting Appointments (medium).js
+++ Python/LC/merge interval/Conflicting Appointments (medium).js
@@ -18,3 +18,50 @@
 // Appointments: [[4,5], [2,3], [3,6]]
 // Output: false
 // Explanation: Since [4,5] and [3,6] overlap, a person cannot attend both of these appointments.
+
+
+class Interval {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    print_interval() {
+        process.stdout.write(`[${this.start}, ${this.end}]`);
+    }
+}
+
+const can_attend_all_appointments = function(intervals) {
+    // TODO: Write your code here
+    if (intervals.length < 2) return true;
+    intervals.sort((a, b) => a.start - b.start);
+    let i = 0,
+        j = 1;
+    while (j < intervals.length) {
+        if (intervals[j].start <= intervals[i].end) {
+            return false
+        };
+        ++i;
+        ++j;
+    }
+    return true;
+};
+
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(1, 4),
+    new Interval(2, 5),
+    new Interval(7, 9),
+  ])}`);
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(6, 7),
+    new Interval(2, 4),
+    new Interval(8, 12),
+  ])}`);
+
+console.log(`Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(4, 5),
+    new Interval(2, 3),
+    new Interval(3, 6),
+  ])}`);
