9ba2c7952d0fef19e5232de0144aa7d0712821bb
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-21 20:03:26 +0100

    working solution

--- Python/LC/merge interval/Employee Free Time (hard).js
+++ Python/LC/merge interval/Employee Free Time (hard).js
@@ -18,3 +18,71 @@
 // Input: Employee Working Hours=[[[1,3]], [[2,4]], [[3,5], [7,9]]]
 // Output: [5,7]
 // Explanation: All employees are free between [5,7].
+
+class Interval {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    get_interval() {
+        return "[" + this.start + ", " + this.end + "]";
+    }
+};
+
+const find_employee_free_time = function(schedule) {
+    result = [],
+        temp = [];
+    for (let i = 0; i < schedule.length; i++) {
+        for (let j = 0; j < schedule[i].length; j++) {
+            temp.push(schedule[i][j]);
+        }
+    }
+    temp.sort((a, b) => a.start - b.start);
+    for (let i = 1; i < temp.length; i++) {
+        if (temp[i].start > temp[i - 1].end) {
+            result.push(new Interval(temp[i - 1].end, temp[i].start))
+        }
+    }
+    return result;
+};
+
+
+input = [
+    [new Interval(1, 3), new Interval(5, 6)],
+    [
+        new Interval(2, 3), new Interval(6, 8)
+    ]
+];
+intervals = find_employee_free_time(input)
+result = "Free intervals: ";
+for (i = 0; i < intervals.length; i++)
+    result += intervals[i].get_interval();
+console.log(result);
+
+
+input = [
+    [new Interval(1, 3), new Interval(9, 12)],
+    [
+        new Interval(2, 4)
+    ],
+    [new Interval(6, 8)]
+];
+intervals = find_employee_free_time(input)
+result = "Free intervals: ";
+for (i = 0; i < intervals.length; i++)
+    result += intervals[i].get_interval();
+console.log(result);
+
+input = [
+    [new Interval(1, 3)],
+    [
+        new Interval(2, 4)
+    ],
+    [new Interval(3, 5), new Interval(7, 9)]
+];
+intervals = find_employee_free_time(input)
+result = "Free intervals: ";
+for (i = 0; i < intervals.length; i++)
+    result += intervals[i].get_interval();
+console.log(result);
