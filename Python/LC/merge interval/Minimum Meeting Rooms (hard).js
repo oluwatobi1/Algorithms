da6a19ade9b597a9b160c1280580e9d74e093c1b
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-21 08:01:05 +0100

    adds solution

--- Python/LC/merge interval/Minimum Meeting Rooms (hard).js
+++ Python/LC/merge interval/Minimum Meeting Rooms (hard).js
@@ -26,3 +26,45 @@
 // Meetings: [[4,5], [2,3], [2,4], [3,5]]
 // Output: 2
 // Explanation: We will need one room for [2,3] and [3,5], and another room for [2,4] and [4,5].
+
+class Meeting {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+};
+
+const min_meeting_rooms = function(meetings) {
+    // TODO: Write your code here
+    let rooms = 1,
+        tempRoom = 1
+    i = 1;
+    if (meetings.length < 2) return rooms;
+    meetings.sort((a, b) => a.start - b.start)
+    let prev = meetings[0];
+    while (i < meetings.length) {
+        if (meetings[i].start < prev.end) {
+            tempRoom++;
+            let start = Math.max(prev.start, meetings[i].start)
+            let end = Math.min(prev.end, meetings[i].end)
+            prev = new Meeting(start, end)
+            rooms = Math.max(tempRoom, rooms)
+        } else {
+            prev = meetings[i]
+            tempRoom = 1;
+        };
+        i++;
+    }
+    return rooms;
+};
+
+
+console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
+      [new Meeting(4, 5), new Meeting(2, 3), new Meeting(2, 4), new Meeting(3, 5)])}`)
+console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
+      [new Meeting(1, 4), new Meeting(2, 5), new Meeting(3, 6), new Meeting(4, 8), new Meeting(7, 9)])}`)
+console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
+      [new Meeting(6, 7), new Meeting(2, 4), new Meeting(8, 12)])}`)
+console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
+      [new Meeting(1, 4), new Meeting(2, 3), new Meeting(3, 6)])}`)
+console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
