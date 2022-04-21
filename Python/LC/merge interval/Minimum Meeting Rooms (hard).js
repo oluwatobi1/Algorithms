611282c8553c36be5f05e7e131648e229b735e94
oluwatobi1 <akintunlesetobi@gmail.com>
2022-04-21 18:09:32 +0100

    fixed bug

--- Python/LC/merge interval/Minimum Meeting Rooms (hard).js
+++ Python/LC/merge interval/Minimum Meeting Rooms (hard).js
@@ -67,4 +67,3 @@ console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
       [new Meeting(6, 7), new Meeting(2, 4), new Meeting(8, 12)])}`)
 console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
       [new Meeting(1, 4), new Meeting(2, 3), new Meeting(3, 6)])}`)
-console.log(`Minimum meeting rooms required: ${min_meeting_rooms(
