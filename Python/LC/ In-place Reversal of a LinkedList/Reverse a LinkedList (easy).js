97ac576d6da1a856f36ca00eda2d63538aeb4cec
oluwatobi1 <akintunlesetobi@gmail.com>
2022-05-02 23:24:02 +0100

    working solution

--- Python/LC/ In-place Reversal of a LinkedList/Reverse a LinkedList (easy).js
+++ Python/LC/ In-place Reversal of a LinkedList/Reverse a LinkedList (easy).js
@@ -1,2 +1,41 @@
 // Given the head of a Singly LinkedList, reverse the LinkedList. Write a
 //  function to return the new head of the reversed LinkedList.
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+
+    get_list() {
+        result = "";
+        temp = this;
+        while (temp !== null) {
+            result += temp.value + " ";
+            temp = temp.next;
+        }
+        return result;
+    }
+};
+
+
+const reverse = function(head) {
+    // TODO: Write your code here
+    let prev = null;
+    while (head) {
+        let next = head.next;
+        head.next = prev
+        prev = head;
+        head = next
+    }
+    return prev;
+};
+
+head = new Node(2);
+head.next = new Node(4);
+head.next.next = new Node(6);
+head.next.next.next = new Node(8);
+head.next.next.next.next = new Node(10);
+
+console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
+console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`)
