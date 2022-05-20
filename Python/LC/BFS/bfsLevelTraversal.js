942007429e67170310a1ce435c4ee974e2173644
oluwatobi1 <akintunlesetobi@gmail.com>
2022-05-20 23:27:56 +0100

    given solution imlpementatino

--- Python/LC/BFS/bfsLevelTraversal.js
+++ Python/LC/BFS/bfsLevelTraversal.js
@@ -1,59 +1,49 @@
-class TreeNode {
+const Deque = require('./collections/deque'); //http://www.collectionsjs.com
 
-    constructor(value) {
-        this.value = value;
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
         this.left = null;
         this.right = null;
     }
-};
+}
 
 
-const traverse = function(root) {
-    result = [
-        [root]
-    ];
-    ans = [
-            [root.value]
-        ]
-        // TODO: Write your code here
-    let node = root,
-        i = 0;
-    while (i < result.length) {
-        let arr = [],
-            temp = [];
-        for (let j = 0; j < result[i].length; j++) {
-            let curr_node = result[i][j];
-            if (curr_node.left) {
-                arr.push(curr_node.left)
-                temp.push(curr_node.left.value)
-            }
-            if (curr_node.right) {
-                arr.push(curr_node.right);
-                temp.push(curr_node.right.value)
-            }
-        }
-        console.log('ans', ans)
-        if (arr.length > 0) {
-            result.push(arr);
-            ans.push(temp)
-        }
-        i++;
+function traverse(root) {
+    result = [];
+    if (root === null) {
+        return result;
     }
-    return ans;
-};
+
+    const queue = new Deque();
+    queue.push(root);
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        currentLevel = [];
+        for (i = 0; i < levelSize; i++) {
+            currentNode = queue.shift();
+            // add the node to the current level
+            currentLevel.push(currentNode.val);
+            // insert the children of current node in the queue
+            if (currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+        result.push(currentLevel);
+    }
+
+    return result;
+}
 
 
-
-var root = new TreeNode(12);
+const root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
 root.right.left = new TreeNode(10);
-root.right.right = new TreeNode(18);
-root.right.right.right = new TreeNode(5);
-root.right.right.right.right = new TreeNode(4);
-root.right.right.right.left = new TreeNode(54);
-root.right.right.right.left.right = new TreeNode(78);
-root.right.right.right.left.right.left = new TreeNode(78);
-root.right.right.right.left.right.left.right = new TreeNode(78);
+root.right.right = new TreeNode(5);
 console.log(`Level order traversal: ${traverse(root)}`);
