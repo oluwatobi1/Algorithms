60c876cd2641d3f2efa80fce00d800d333f0a5a2
oluwatobi1 <akintunlesetobi@gmail.com>
2022-05-27 18:52:35 +0100

    given solution

--- Python/LC/BFS/Minimum Depth of a Binary Tree.js
+++ Python/LC/BFS/Minimum Depth of a Binary Tree.js
@@ -1,8 +1,3 @@
-// Find the minimum depth of a binary tree. The minimum depth is the number of 
-// nodes along the shortest path from the root node to the nearest leaf node.
-
-
-
 class TreeNode {
 
     constructor(value) {
@@ -15,31 +10,24 @@ class TreeNode {
 
 const find_minimum_depth = function(root) {
     // TODO: Write your code here
-    let result = 1;
+    let result = 0;
     let queue = [root];
     while (queue.length > 0) {
+        result += 1
         let levelSize = queue.length;
-        let right = true,
-            left = true;
         for (let i = 0; i < levelSize; i++) {
             currLeaf = queue.shift()
+            if (!currLeaf.left && !currLeaf.right) {
+                return result
+            }
             if (currLeaf.left) {
                 queue.push(currLeaf.left)
-                left = true
-            } else {
-                left = false
+
             }
             if (currLeaf.right) {
                 queue.push(currLeaf.right)
-                right = true
-            } else {
-                right = false
-            }
-            if (!right && !left) {
-                return result
             }
         }
-        result += 1
     }
     return result;
 };
